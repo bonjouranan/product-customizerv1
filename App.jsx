346d0fb6075953ec232b57057afe331675c25ddd
bonjouranan <3996161@gmail.com>
2025-04-16 16:18:23 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { useState, useMemo } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import * as THREE from "three";
+import { useRef, useEffect } from "react";
+
+/* Placeholder for vape customizer content */
+export default function ProductCustomizer() {
+  return <div>Placeholder for full product customizer code</div>;
+}
